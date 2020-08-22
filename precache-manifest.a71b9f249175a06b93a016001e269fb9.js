99922300b20ca7e286d2203f19972b74c2a444fa
ushieru <uzielcocolan@gmail.com>
2020-08-22 14:29:55 -0500

    Updates

---
+++ precache-manifest.a71b9f249175a06b93a016001e269fb9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37f9644f9377b2415aaf6de6a913d58",
+    "url": "/healthy-app/index.html"
+  },
+  {
+    "revision": "2bc9ee8b331317a1c0f1",
+    "url": "/healthy-app/static/css/2.4e8fb8c5.chunk.css"
+  },
+  {
+    "revision": "ffde8abb62b2fa48216d",
+    "url": "/healthy-app/static/css/main.5bf95be6.chunk.css"
+  },
+  {
+    "revision": "2bc9ee8b331317a1c0f1",
+    "url": "/healthy-app/static/js/2.89b5abd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/healthy-app/static/js/2.89b5abd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffde8abb62b2fa48216d",
+    "url": "/healthy-app/static/js/main.76f3582e.chunk.js"
+  },
+  {
+    "revision": "6f1a771482fc932f15a3",
+    "url": "/healthy-app/static/js/runtime-main.89af4818.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/healthy-app/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/healthy-app/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/healthy-app/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/healthy-app/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "25560fbb2e6d4f6285a4912f7b732ddb",
+    "url": "/healthy-app/static/media/We-take-care-of-health.25560fbb.svg"
+  },
+  {
+    "revision": "b6e1c2f15edc731746081104021dfdc9",
+    "url": "/healthy-app/static/media/vegetable.b6e1c2f1.svg"
+  }
+]);
