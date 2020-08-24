07799389c2558c1e0889ba7c2625848aa4e07411
ushieru <uzielcocolan@gmail.com>
2020-08-23 20:51:46 -0500

    add mobile options 📱

--- src/pages/Admin/Patients/Patients.jsx
+++ src/pages/Admin/Patients/Patients.jsx
@@ -13,7 +13,16 @@ export default function Patients() {
     return <>
         <div className="mx-5 sm:mx-12 lg:mx-20 xl:mx-64">
             <div className="flex flex-wrap -mx-2">
-
+                <div className="w-full mb-5">
+                    <div className="flex">
+                        <div className="hidden lg:block lg:w-1/12 px-2">
+                            <button className="bg-green-500 text-white w-full h-full rounded-full"><i className="material-icons align-middle">person_add</i></button>
+                        </div>
+                        <div className="w-full lg:w-11/12">
+                            <input placeholder="🔎 Buscar" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" />
+                        </div>
+                    </div>
+                </div>
                 <div className="w-full lg:w-1/2 xl:w-1/3 mb-4 px-2">
                     <div className="bg-gray-200 rounded-md w-full hover:shadow-md p-3">
                         <div className="flex -mx-1 md:-mx-2">
@@ -103,8 +112,8 @@ export default function Patients() {
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
+        <button className=" lg:hidden fixed bottom-0 right-0 mb-6 mr-6 bg-green-500 text-white rounded-full w-16 h-16"><i style={{fontSize: '30px'}} className="material-icons align-middle">person_add</i></button>
     </>;
 }
